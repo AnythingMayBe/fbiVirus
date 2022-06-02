7c3053f6b8eafa194d9440f42c7910aa022d67f0
AnythingMayBe <anythingmaybe@protonmail.com>
2022-06-02 18:50:17 +0200

    Added waiting before the sound is eared

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from playsound import playsound
+import time
+import random
 
 class Virus:
     def __init__(self):
         self.config = {
-            "audioPath": "audio.mp3" # The video you want to ear
+            "audioPath": "audio.mp3", # The video you want to ear,
+            "minTimeToWait": 10, # The minimum time in seconds you want to wait before you can ear the sound
+            "maxTimeToWait": 30 # The maximum time in seconds you want to wait before you can ear the sound
         }
     
     def earAudio(self):
         playsound(self.config["audioPath"])
+    
+    def waitBeforeEaring(self):
+        time.sleep(random.randint(self.config["minTimeToWait"], self.config["minTimeToWait"]))
+        self.earAudio()
 
 if __name__ == "__main__":
     instance = Virus()
-    instance.earAudio()
+    instance.waitBeforeEaring()
