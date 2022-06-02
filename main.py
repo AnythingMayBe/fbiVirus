af8f6384a0015a01b172a97b52b405e8f34833f3
AnythingMayBe <anythingmaybe@protonmail.com>
2022-06-02 18:44:27 +0200

    Added config for main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+class Virus:
+    def __init__(self):
+        config = {
+            "audioPath": "audi.mp3" # The video you want to ear
+        }
+
+if __name__ == "__main__":
+    instance = Virus()
