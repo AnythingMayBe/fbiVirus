4f72d81a4a23cdd23d908d5fe224e9bb764bf0de
AnythingMayBe <anythingmaybe@protonmail.com>
2022-06-02 18:47:50 +0200

    Added function to ear the audio

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from playsound import playsound
+
 class Virus:
     def __init__(self):
-        config = {
-            "audioPath": "audi.mp3" # The video you want to ear
+        self.config = {
+            "audioPath": "audio.mp3" # The video you want to ear
         }
+    
+    def earAudio(self):
+        playsound(self.config["audioPath"])
 
 if __name__ == "__main__":
     instance = Virus()
+    instance.earAudio()
